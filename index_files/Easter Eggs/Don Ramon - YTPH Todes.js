c7c4bfc10f31c8f18fe22a408d642ed408632f19
Xion-14 <64810647+Xion-14@users.noreply.github.com>
2020-11-05 22:14:24 +0100

    Update Don Ramon - YTPH Todes.js

--- index_files/Easter Eggs/Don Ramon - YTPH Todes.js
+++ index_files/Easter Eggs/Don Ramon - YTPH Todes.js
@@ -3,7 +3,7 @@
 function myDonRamon() {
  if(buscador.innerText == "todes" || buscador.innerText == "TODES" || buscador.innerText == "Todes") {
     easter_egg.src = "index_files/Easter Eggs/Don Ramon - YTPH Todes.mp4";
-    StartEasterEgg(); 
+    StartEasterEgg();
     EndEasterEgg();
  }
 }
