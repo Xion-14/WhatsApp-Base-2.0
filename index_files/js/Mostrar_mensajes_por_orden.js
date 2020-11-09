094c840680cff9086171d9cc1d239c509416e1ef
Xion-14 <64810647+Xion-14@users.noreply.github.com>
2020-11-09 18:22:56 +0100

    Ocultar/MostrarMensajes() actualizado
    Se ha cambiado la forma en la que se muestran y ocultan los mensaje con la funciones "OcultarMensajes()" y "MostrarMensaje()".
    Ahora se añade la clase expuesto para mostrar y se pone la clase "oculto" para "ocultar" en lugar ser simplemente indicativos. Y se han puesto comentarios para la mejor compresión del codigo.

--- index_files/js/Mostrar_mensajes_por_orden.js
+++ index_files/js/Mostrar_mensajes_por_orden.js
@@ -1,56 +1,34 @@
-// Botón que hace aparecer los mensajes que estan ocultos 1 a 1
-
-var mensajes_chat = document.getElementsByClassName("mensaje");
-var mensajes_ocultos = document.getElementsByClassName("oculto");
-var r = -1;
-
 // Variable que indica el chat en el que se encuentra el usuario
 var chat_index = 0;
 
-/*
-Función que añade la clase 'expuesto' a los mensajes que tienen 'display = ""'
-y la clase 'oculto' a los que tiene 'display = "none"'
-*/
-
-function AddClassHidden() {
-    i = 0;
-
-    for ( i = 0 ; i <= mensajes_chat.length-1 ; i++ ) {
-       if(mensajes_chat[i].style.display === "none") {
-          mensajes_chat[i].classList.add('oculto');
-          mensajes_chat[i].classList.remove('expuesto');
-
-       } else { mensajes_chat[i].classList.add('expuesto');
-	 mensajes_chat[i].classList.remove('oculto');
-         }
-    }
-}
-
-setInterval(AddClassHidden, 1);
+var mensajes_chat = document.getElementsByClassName("mensaje");
 
 // Mostrar los mensajes ocultos del chat seleccionado 1 detrás de otro
 
   var chats = document.getElementsByClassName("chat");
 
+  var n1 = -1; // Limitador para mostrar mensajes 1 a 1
 
-  var n1 = -1;
-
+// Hace aparecer los mensajes que estan ocultos 1 a 1
 function MostrarMensaje() {
 
   var n = 0;
-  var o = 0;
+  var o = 0; // 
 
  for(o = 0; o <= chats[chat_index].children.length-1; o++) {
    if(chats[chat_index].children[o].classList[3] === "oculto" || chats[chat_index].children[o].classList[4] === "oculto") {
       n1 = o;
-      break;
+      break; // Cada vez que encuentra, de entre todos los mensajes del chat, un mensaje con la clase 'oculto',
+             // se incrementa n1 hasta el valor de la posición de ese mensaje y se sale del bucle
    }
  }
 
+// Busca el 1er mensaje con la clase 'oculto' del chat y le pone la clase 'expuesto' en su lugar (conseguido gracias a la limitación impuesta por n1)
  for(n = 0; n <= n1; n++) {
-    if(chats[chat_index].children[n].classList[3] === "oculto" || chats[chat_index].children[n].classList[4] === "oculto") {
-       chats[chat_index].children[n].style.display = "";
-    }
+   if(chats[chat_index].children[n].classList[3] === "oculto" || chats[chat_index].children[n].classList[4] === "oculto") {
+      chats[chat_index].children[n].classList.remove("oculto");
+      chats[chat_index].children[n].classList.add("expuesto");
+   }
  }
 }
 
@@ -61,8 +39,9 @@ function OcultarMensajes() {
   var m1 = mensajes_chat;
   
   for ( i = 2 ; i <= m1.length-1 ; i++ ) {
-    
-   chats[chat_index].children[i].style.display = "none";
-    
+   if(chats[chat_index].children[i] != undefined) {
+      chats[chat_index].children[i].classList.remove("expuesto");
+      chats[chat_index].children[i].classList.add("oculto");
+   }
   }
 }
