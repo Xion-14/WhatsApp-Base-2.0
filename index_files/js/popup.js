24716f17ab4c26a24e776c87098c36e890c577cc
Xion-14 <64810647+Xion-14@users.noreply.github.com>
2020-11-17 14:52:25 +0100

    Video

--- index_files/js/popup.js
+++ index_files/js/popup.js
@@ -260,7 +260,7 @@ function PopupOptions2() {
 
 function DeletePopupOptions() {
   // Evitar que se le de a más de un botón durante la desaparición del Popup
-  if(span_popup[3] != undefined) {
+  if(span_popup[3].children[0] != undefined) {
     for(var i = 0; i < span_popup[3].children[0].children[0].children.length; i++) {
         span_popup[3].children[0].children[0].children[i].children[0].removeAttribute("onclick");
     }
